eff24d512138f9b027d94c7b9ae62cb05a19f9ec
Jan Rosa <jrosa@student.agh.edu.pl>
2025-10-20 10:23:14 +0000

    fixed matmul on t4

--- CUDA_Lab02/mult-mv/mult_mv.cu
+++ CUDA_Lab02/mult-mv/mult_mv.cu
@@ -2,49 +2,68 @@
 
 #define GET_IDX_ROW_MAJOR(width, x, y) (((y) * (width)) + (x))
 
-__global__ void multMatrixVector(float *b, float *A, float *x, unsigned int nrows, unsigned int ncols)
+__global__ void multMatrixVector(float *b, const float *A, const float *x,
+                                 unsigned int nrows, unsigned int ncols)
 {
     int row = threadIdx.x + blockIdx.x * blockDim.x;
-    if (!(row < nrows))
+    if (row >= nrows) return;
+
+    float b_accum = 0.0f;
+
+    // Multiply each element in the row of A by the corresponding element in x
+    for (unsigned int col = 0; col < ncols; ++col)
     {
-        return;
+        b_accum += A[GET_IDX_ROW_MAJOR(ncols, col, row)] * x[col];
     }
 
-    float b_accum = 0;
+    // Debug print (optional)
+    // printf("b[%d] = %f\n", row, b_accum);
 
-    for (size_t col = 0; col < ncols; col++)
-    {
-        b_accum = A[GET_IDX_ROW_MAJOR(ncols, col, row)]*x[row];
-    }
-    
     b[row] = b_accum;
 }
 
 Matrix multMatrixVectorOnDevice(const Matrix &A, const Matrix &x)
 {
-    Matrix b = Matrix(x);
+    if (A.getCols() != x.getRows())
+    {
+        throw std::runtime_error("Matrix and vector dimensions do not match for multiplication.");
+    }
 
-    // allocate input and output images in the device
-    float *d_A;
-    float *d_x;
-    float *d_b;
-    cudaMalloc((void **)&d_A, A.getRows() * A.getCols() * sizeof(float));
-    cudaMalloc((void **)&d_x, x.getRows() * x.getCols() * sizeof(float));
-    cudaMalloc((void **)&d_b, b.getRows() * b.getCols() * sizeof(float));
+    Matrix b(A.getRows(), 1);
 
-    // copy image to the device
-    cudaMemcpy(d_A, A.getDataConstPtr(), A.getRows() * A.getCols() * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_x, A.getDataConstPtr(), x.getRows() * x.getCols() * sizeof(float), cudaMemcpyHostToDevice);
+    // Device memory
+    float *d_A = nullptr;
+    float *d_x = nullptr;
+    float *d_b = nullptr;
 
-    dim3 dimGrid(ceil((float)A.getRows()), 1);
-    dim3 dimBlock(1, 1, 1);
-    multMatrixVector<<<dimGrid, dimBlock>>>(d_b, d_A, d_x, A.getRows(), A.getCols());
+    size_t sizeA = A.getRows() * A.getCols() * sizeof(float);
+    size_t sizeX = x.getRows() * x.getCols() * sizeof(float);
+    size_t sizeB = b.getRows() * b.getCols() * sizeof(float);
 
-    cudaMemcpy(b.getDataPtr(), d_b, b.getRows() * b.getCols() * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMalloc((void **)&d_A, sizeA);
+    cudaMalloc((void **)&d_x, sizeX);
+    cudaMalloc((void **)&d_b, sizeB);
 
+    // Copy data to device
+    cudaMemcpy(d_A, A.getDataConstPtr(), sizeA, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_x, x.getDataConstPtr(), sizeX, cudaMemcpyHostToDevice);
+
+    // Kernel launch configuration
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (A.getRows() + threadsPerBlock - 1) / threadsPerBlock;
+
+    printf("Entering Kernel\n");
+    multMatrixVector<<<blocksPerGrid, threadsPerBlock>>>(d_b, d_A, d_x, A.getRows(), A.getCols());
+    cudaDeviceSynchronize();  // Ensure kernel completes before copying
+    printf("Leaving Kernel\n");
+
+    // Copy result back
+    cudaMemcpy(b.getDataPtr(), d_b, sizeB, cudaMemcpyDeviceToHost);
+
+    // Free device memory
     cudaFree(d_A);
-    cudaFree(d_b);
     cudaFree(d_x);
+    cudaFree(d_b);
 
     return b;
 }
